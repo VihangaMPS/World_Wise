4bf8c09f24c8ea7435249306164dfbe032b990b2
MPS Vihanga <>
2024-03-31 23:41:21 +0530

    Routing project structure created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Product from "./pages/Product.jsx";
+import Pricing from "./pages/Pricing.jsx";
+import Homepage from "./pages/Homepage.jsx";
+import NotFoundPage from "./pages/NotFoundPage.jsx";
+import AppLayout from "./pages/AppLayout.jsx";
+import Login from "./pages/Login.jsx";
+
 function App() {
 
     return (
-        <div>Hello</div>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Homepage/>}/>
+                <Route path="product" element={<Product/>}/>
+                <Route path="pricing" element={<Pricing/>}/>
+                <Route path="login" element={<Login/>}/>
+                <Route path='app' element={<AppLayout/>}/>
+                <Route path="*" element={<NotFoundPage/>}/>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
 export default App;
+
+//7
